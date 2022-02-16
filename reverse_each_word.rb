286fe7cace3aa4efc37a6ff5090a69ee34a1e51b
Derek Araujo <derekaraujo19@gmail.com>
2022-02-16 15:53:02 -0500

    Lab Complete

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'pry'
+# Split the string on " " to get access to each word in the sentence
+# Reverse each word, and add it to a new array
+# Join the array of words on " " to create one string
+
+def reverse_each_word(sentence)
+#   words = sentence.split
+
+#   # reversed_words = []
+
+#   # words.each do |word|
+#   #   reversed_words << word.reverse
+#   # end
+
+#   reversed_words = words.map(&:reverse)
+
+#   reversed_words.join(" ")
+# end
+
+  sentence.split.map(&:reverse).join(" ")
+
+end
+puts reverse_each_word("Hello there, and how are you?")
